4f2a15125b2aac9585563ca6fba2eabc597c92ae
kimyihyung <kimlh3743@gmail.com>
2022-11-14 14:32:29 +0900

    .

--- javascript/assets/js/tetris copy 2.js
+++ javascript/assets/js/tetris copy 2.js
@@ -1,38 +1,22 @@
-// 선택자
 const tetrisWrap = document.querySelector(".tetris__wrap");
-const playGround = tetrisWrap.querySelector(".playground > ul");
-const TstartBtn = tetrisWrap.querySelector(".tetris__start");
-const Tmsg = tetrisWrap.querySelector(".tetris__msg");
-const tetrisScore = tetrisWrap.querySelector(".tetris__score");
-
-// 변수설정
-let rows = 15; // 가로줄 만들기
-let cols = 10; // 세로줄 만들기
-let Tscore = 0;
+const playground = tetrisWrap.querySelector(".playground > ul");
+const allCannes = playground.querySelectorAll("li ul li");
+// variables
+let rows = 15;
+let cols = 12;
+let tetrisScore = 0;
+let tetrisLine = 0;
 let duration = 500;
 let downInterval;
 let tempMovingItem;
-let tID = 0;
-let gameover = false;
-
-let Tsound = [
-  "../assets/audio/success.mp3",
-  "../assets/audio/match.mp3",
-  "../assets/audio/unmatch.mp3",
-];
-let TsoundBg = new Audio(Tsound[0]);
-let TsoundMatch = new Audio(Tsound[1]);
-let TsoundUnMatch = new Audio(Tsound[2]);
-
-// 블록정보
+// 블록 정보
 const movingItem = {
-  type: "",
-  direction: 0, // 블록 모양
+  type: "Imino", // 블록 이름
+  direction: 0, //블록 모양
   top: 0,
-  left: 0,
+  left: 6,
 };
-
-// 블록의 좌표값
+// 블록 좌표값
 const blocks = {
   Tmino: [
     [
@@ -218,108 +202,100 @@ const blocks = {
   ],
 };
 
+for (let i = 0; i < rows; i++) {
+  prependNewLine(); //블록 라인 만들기
+}
 // 시작하기
 function init() {
-  gameover = false;
-  TsoundBg.play();
-  playGround.innerHTML = "";
-  tetrisScore.innerText = "";
-  Tscore = 0;
-  duration = 500;
+  // 블록 정보를 tempMovingItem에 입력
   tempMovingItem = { ...movingItem };
-  for (let i = 0; i < rows; i++) {
-    prependNewLine(); // 라인 만들기
-  }
-  generateNewBlock(); // 블럭만들기
 
-  Tmsg.classList.remove("show");
+  allCannes.forEach((e) => {
+    e.removeAttribute("class");
+  });
+
+  // renderBlocks(); //블록 출력하기
+  // moveBlock();
+  generateNewBlock(); //블록 만들기
 }
-TstartBtn.removeEventListener("click", init);
-
-// 블록 만들기
+// 블록 만들기 : cols * rows
 function prependNewLine() {
+  // createElement : HTML 태그를 만듦
   const li = document.createElement("li");
   const ul = document.createElement("ul");
   for (let j = 0; j < cols; j++) {
     const matrix = document.createElement("li");
-
     ul.prepend(matrix);
   }
-
+  // prepend : 가장 마지막에 넣음
   li.prepend(ul);
-  playGround.prepend(li);
+  playground.prepend(li);
 }
-
 // 블록 출력하기
 function renderBlocks(moveType = "") {
-  if (gameover) return;
-  const { type, direction: dir, left, top } = tempMovingItem;
+  // moveType 매개변수를 추후에 추가했음. 만약 전달할 값이 없으면 위처럼 적으면 됨
+
+  // const ty = tempMovingItem.type;
+  // const di = tempMovingItem.direction;
+  // const to = tempMovingItem.top;
+  // const le = tempMovingItem.left;
+  // 아래와 같이 간단하게 작업할 수 있음... 간단한가...
+  // 1. 블록 모양잡기
+  const { type, direction, top, left } = tempMovingItem;
+
+  // 3. 블록 움직이게 하기(moving 클래스가 없으면 복제됨)
   const movingBlocks = document.querySelectorAll(".moving");
   movingBlocks.forEach((moving) => {
     moving.classList.remove(type, "moving");
   });
 
-  blocks[type][dir].some((block) => {
-    const x = block[0] + left;
-    const y = block[1] + top;
-
-    target = playGround.children[y]
-      ? playGround.children[y].children[0].children[x]
+  // 1. 블록 모양잡기
+  // forEach, some의 차이 : some은 중간에 멈출 수 있음, forEach는 계속 반복
+  blocks[type][direction].some((block) => {
+    const x = block[0] + left; //2 0 1 1
+    const y = block[1] + top; //1 1 0 1
+    // 4. 블록이 영역을 벗어나는 것 감지
+    const target = playground.childNodes[y]
+      ? playground.childNodes[y].childNodes[0].childNodes[x]
       : null;
     const isAvailable = checkEmpty(target);
-
-    if (isAvailable) target.classList.add(type, "moving");
-    else {
-      // 끝에 닿았다
+    if (isAvailable) {
+      target.classList.add(type, "moving");
+    } else {
       tempMovingItem = { ...movingItem };
-      tID = setTimeout(() => {
+
+      setTimeout(() => {
         renderBlocks();
         if (moveType === "top") {
-          clearTimeout(tID);
           seizeBlock();
         }
       }, 0);
       return true;
     }
+    // console.log({ playground });
   });
-
   movingItem.left = left;
   movingItem.top = top;
-  movingItem.direction = dir;
+  movingItem.direction = direction;
+
+  // 2. 블록의 이름(모양)에 맞추어 클래스 추가 -> 해당하는 부분에 색칠됨
+  // target.classList.add(type, "moving");
+  // 를 했었지만 4번으로 이동
 }
 
-// 블록 감지하기
+//블록 감지하기
 function seizeBlock() {
-  clearTimeout(tID);
   const movingBlocks = document.querySelectorAll(".moving");
   movingBlocks.forEach((moving) => {
     moving.classList.remove("moving");
     moving.classList.add("seized");
   });
-
-  // 겜 졌는가 판단
-  checkLose();
-
-  // 점수 계산
   checkMatch();
-}
-
-// 졌는가
-function checkLose() {
-  const childNodes = Array.from(playGround.children)[0].querySelectorAll(
-    "ul li"
-  );
-  childNodes.forEach((ch) => {
-    if (ch.classList.contains("seized")) {
-      gameover = true;
-      TsoundUnMatch;
-    }
-  });
 }
 
 // 한줄 제거하기
 function checkMatch() {
-  const childNodes = Array.from(playGround.children);
+  const childNodes = playground.childNodes;
   childNodes.forEach((child) => {
     let matched = true;
     child.children[0].childNodes.forEach((li) => {
@@ -330,94 +306,100 @@ function checkMatch() {
     if (matched) {
       child.remove();
       prependNewLine();
-      Tscore++;
-      TsoundMatch.play();
-      duration = duration - 5;
-
-      tetrisScore.innerText = `점수 : ${Tscore}`;
+      tetrisScore += 10;
+      tetrisLine++;
+      document.querySelector(".tetris__score span").innerHTML = tetrisScore;
+      document.querySelector(".tetris__line span").innerHTML = tetrisLine;
     }
   });
   generateNewBlock();
 }
-// 새로운 블럭 만들기
+
+//새로운 블록 만들기
 function generateNewBlock() {
-  if (gameover) {
-    TsoundBg.pause();
-    Tmsg.classList.add("show");
-    Tmsg.innerHTML = `
-       ${Tscore}점
-      `;
-    TstartBtn.addEventListener("click", init);
-    // TstartBtn.onclick(init);
-    return;
-  }
   clearInterval(downInterval);
+
   downInterval = setInterval(() => {
+    // 점점 빨라지는 것 방지하기 위해 downInterval 만들어줌
     moveBlock("top", 1);
   }, duration);
 
   const blockArray = Object.entries(blocks);
   const randomIndex = Math.floor(Math.random() * blockArray.length);
   movingItem.type = blockArray[randomIndex][0];
+
   movingItem.top = 0;
-  movingItem.left = 4;
+  movingItem.left = 6;
   movingItem.direction = 0;
   tempMovingItem = { ...movingItem };
+
   renderBlocks();
 }
 
-// 빈칸 확인하기
+// 빈칸 감지
 function checkEmpty(target) {
   if (!target || target.classList.contains("seized")) {
-    return;
+    // 빈칸이 없으면 종료
+    return false;
   }
   return true;
 }
-
 // 블록 움직이기
 function moveBlock(moveType, amount) {
   tempMovingItem[moveType] += amount;
   renderBlocks(moveType);
 }
 
-// 방향 바꾸기
+//모양 바꾸기
 function changeDirection() {
   const direction = tempMovingItem.direction;
-  direction === 3 ? (tempMovingItem.direction = 0) : tempMovingItem.direction++;
+  direction === 3
+    ? (tempMovingItem.direction = 0)
+    : (tempMovingItem.direction += 1);
+
   renderBlocks();
 }
 
-// 빨리 내리기
+//빨리 내리기
 function dropBlock() {
   clearInterval(downInterval);
+
   downInterval = setInterval(() => {
     moveBlock("top", 1);
   }, 10);
-  renderBlocks();
 }
+
 // 이벤트
 document.addEventListener("keydown", (e) => {
   switch (e.keyCode) {
-    case 32:
-      dropBlock();
-      break;
-    case 37:
-      moveBlock("left", -1);
-      break;
-
-    case 38:
-      changeDirection();
-      break;
-
+    // 오른쪽 방향키를 눌렀을 때, left 1 움직여라
     case 39:
       moveBlock("left", 1);
       break;
-
+    // 왼쪽
+    case 37:
+      moveBlock("left", -1);
+      break;
+    // 아래쪽
     case 40:
       moveBlock("top", 1);
+      break;
+    // 위쪽
+    case 38:
+      changeDirection();
+      break;
+    // 내려찍기
+    case 32:
+      dropBlock();
       break;
 
     default:
       break;
   }
+});
+document.querySelector(".tetris-start").addEventListener("click", () => {
+  document.querySelector(".tetris__main").style.display = "none";
+  document.querySelector(".tetris__inner").style.display = "block";
+  document.querySelector(".tetris__info").style.display = "block";
+  init();
 });
